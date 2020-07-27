341c49157520183180620def59b0dca6d7568dff
Ibrahim Elsayed <ibrahim.elsayed.26498@gmail.com>
2020-07-27 20:34:52 +0200

    Cutomer Area: Shop page Added, need to be modified

---
+++ EcommerceProject/Controllers/ShopController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using EcommerceProject.Models;
+using EcommerceProject.DAL;
+using EcommerceProject.VM;
+
+namespace EcommerceProject.Controllers
+{
+    public class ShopController : Controller
+    {
+        ProductDAL productDAL = new ProductDAL();
+        // GET: Shop
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public PartialViewResult products()
+        {
+            return PartialView(productDAL.GetAll());
+        }
+        public PartialViewResult category()
+        {
+            return PartialView(new CategoryDAL().GetAll());
+        }
+
+        public PartialViewResult brands()
+        {
+            return PartialView(new BrandDAL().GetAll());
+        }
+        
+        public PartialViewResult filterByPrice()
+        {
+            return PartialView();
+        }
+
+        public PartialViewResult filterByColor()
+        {
+            return PartialView();
+        }
+    }
+}
